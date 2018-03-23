9ac967586ab2ee9ab0706b7539acb87e731d6db1
unknown <1242924472@qq.com>
2018-03-23 16:06:35 +0800

    首次提交

---
+++ sendmail.py
@@ -0,0 +1,25 @@
+#coding: utf-8
+import smtplib
+from email.mime.text import MIMEText
+from email.utils import formataddr
+ 
+my_sender='1242924472@qq.com'    # 发件人邮箱账号
+my_pass = 'tfxaawthzgimhhjb'              # 发件人邮箱密码(此处是授权码，不是QQ密码)
+#my_user ='1796787430@qq.com'      # 收件人邮箱账号
+
+#title = '这是一个用Python发出来的邮件'       #邮件标题
+def sendmail(my_user,text,title,sname):
+    ret=True
+    try:
+        msg=MIMEText(text,'plain','utf-8')
+        msg['From']=formataddr([sname,my_sender])  # 括号里的对应发件人邮箱昵称、发件人邮箱账号
+        msg['To']=formataddr(["",my_user])              # 括号里的对应收件人邮箱昵称、收件人邮箱账号
+        msg['Subject']=title                # 邮件的主题，也可以说是标题
+ 
+        server=smtplib.SMTP_SSL("smtp.qq.com", 465)  # 发件人邮箱中的SMTP服务器，端口是25
+        server.login(my_sender, my_pass)  # 括号中对应的是发件人邮箱账号、邮箱密码
+        server.sendmail(my_sender,my_user,msg.as_string())  # 括号中对应的是发件人邮箱账号、收件人邮箱账号、发送邮件
+        server.quit()  # 关闭连接
+    except Exception:  # 如果 try 中的语句没有执行，则会执行下面的 ret=False
+        ret=False
+    return ret
---
+++ spider.py
@@ -0,0 +1,71 @@
+#code:utf-8
+from dao import mydao
+from sendmail import *
+import time
+import requests
+from bs4 import BeautifulSoup
+import json
+session = requests.session()
+def spider(kskm,kc,email):
+    currenttime = time.localtime()
+    startTime = time.strftime("%Y-%m-%d", currenttime)
+    year = time.strftime("%Y",currenttime)
+    month = time.strftime("%m",currenttime)
+    day = time.strftime("%d",currenttime)
+    
+    month = str(int(month) + 2)
+    endTime = year + '-' + month + '-' + day
+    
+    dataurl = 'http://hb.122.gov.cn/m/examplan/getExamPlan'
+    detailurl = 'http://hb.122.gov.cn/m/examplan/getExamPlanDetail'
+    data = {
+        'endTime':endTime,
+        'fzjg':'鄂G',
+        'kscx':'C1',
+        'ksdd':'',
+        'kskm':kskm,         #考试科目
+        'page':0,
+        'startTime':startTime,
+        'zt':0
+    }
+    try:
+        resp = session.post(dataurl,data=data)
+        soup = BeautifulSoup(resp.text, 'html.parser')
+    except:
+        print('出问题了')
+    myjson = json.loads(str(soup))
+    if len(myjson) == 3:
+        for i in range(len(myjson['data']['content'])):
+            kcmc = myjson['data']['content'][i]['kcmc']    #考场名称
+            if kc in kcmc:
+                data['ksdd'] = myjson['data']['content'][i]['kcdddh']    #给post数据赋新值
+                try:
+                    resp = session.post(detailurl,data=data)
+                    soup = BeautifulSoup(resp.text, 'html.parser')
+                    detailjson = json.loads(str(soup))
+                except:
+                    print('Detail出问题了')
+                text = ''
+                kcmc = ''
+                ksrq = ''
+                flag = 0
+                for j in range(len(detailjson['data'])):
+                    kcmc = detailjson['data'][j]['kcmc']  #考场名称
+                    ksrq = detailjson['data'][j]['ksrq']  #考试日期
+                    sqrs = detailjson['data'][j]['sqrs']  #预约人数
+                    if (int(sqrs) != 0):
+                        if mydao.is_new(kcmc,ksrq,kskm) == True:
+                            text = text + kcmc + ' ' + ksrq + ' ' + detailjson['data'][j]['kscx'] + ' 科目'+str(kskm)+'\n'
+                            flag = 1
+                            mydao.insert_result(kcmc,ksrq,kskm)
+                            print (kcmc,ksrq,sqrs)
+                print(text)
+                if flag == 1:
+                    sendmail(email,text,'有新的科目'+str(kskm)+'考试安排出来了',' ')
+            
+if __name__ == '__main__':
+    users = mydao.select_user()
+    print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+    for user in users:
+        spider(user['kskm'],user['kcmc'],user['email'])
+
