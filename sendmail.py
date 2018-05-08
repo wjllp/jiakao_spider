c1c211b2c56b14ad68c89bd4f4d00eeec56f8fe4
wjllp <1242924472@qq.com>
2018-05-08 20:06:35 +0800

    修改部分内容

--- sendmail.py
+++ sendmail.py
@@ -5,9 +5,7 @@ from email.utils import formataddr
  
 my_sender='1242924472@qq.com'    # 发件人邮箱账号
 my_pass = 'tfxaawthzgimhhjb'              # 发件人邮箱密码(此处是授权码，不是QQ密码)
-#my_user ='1796787430@qq.com'      # 收件人邮箱账号
 
-#title = '这是一个用Python发出来的邮件'       #邮件标题
 def sendmail(my_user,text,title,sname):
     ret=True
     try:
@@ -16,7 +14,7 @@ def sendmail(my_user,text,title,sname):
         msg['To']=formataddr(["",my_user])              # 括号里的对应收件人邮箱昵称、收件人邮箱账号
         msg['Subject']=title                # 邮件的主题，也可以说是标题
  
-        server=smtplib.SMTP_SSL("smtp.qq.com", 465)  # 发件人邮箱中的SMTP服务器，端口是25
+        server=smtplib.SMTP_SSL("smtp.qq.com", 465)  # 发件人邮箱中的SMTP服务器，端口是465
         server.login(my_sender, my_pass)  # 括号中对应的是发件人邮箱账号、邮箱密码
         server.sendmail(my_sender,my_user,msg.as_string())  # 括号中对应的是发件人邮箱账号、收件人邮箱账号、发送邮件
         server.quit()  # 关闭连接
